de1939eb05b7a010a1066e0f382a486c2e98de3a
KheyliVelez <ktvelez65@misena.edu.co>
2023-10-10 17:48:22 -0500

    favorite

--- app/src/main/java/com/example/radcooksplash/MainActivity.kt
+++ app/src/main/java/com/example/radcooksplash/MainActivity.kt
@@ -1,19 +1,52 @@
 package com.example.radcooksplash
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //search
         val searchCard = findViewById<CardView>(R.id.searchCard)
         searchCard.setOnClickListener {
             val intent = Intent(this@MainActivity, search::class.java)
             startActivity(intent)
         }
+        //foro
+        val forumCard = findViewById<CardView>(R.id.forumCard)
+        forumCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, Forum::class.java)
+            startActivity(intent)
+        }
+
+        //ingresar receta
+        val enterrecipeCard = findViewById<CardView>(R.id.enterrecipeCard)
+        enterrecipeCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, EnterRecipe::class.java)
+            startActivity(intent)
+        }
+
+        //ingresar ingrediente
+
+        val enteringredientCard = findViewById<CardView>(R.id.enteringredientCard)
+        enteringredientCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, Ingredient::class.java)
+            startActivity(intent)
+        }
+
+        //favoritos
+        val favoriteCard = findViewById<CardView>(R.id.favoritetCard)
+        favoriteCard.setOnClickListener {
+            val intent = Intent(this@MainActivity, Favorite::class.java)
+            startActivity(intent)
+        }
+
     }
 }
+
